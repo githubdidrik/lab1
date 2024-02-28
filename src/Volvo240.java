dbaf9dbee7a0b81d668336c1bd1c120695fc7525
githubdidrik <didrik.n@gmail.com>
2024-02-28 19:17:37 +0100

    fix

--- src/Volvo240.java
+++ src/Volvo240.java
@@ -7,9 +7,6 @@ import java.io.IOException;
 public class Volvo240 extends Car {
     private final static double trimFactor = 1.25;
     private BufferedImage image;
-
-
-
     public Volvo240(){
         super("Volvo240", Color.black, 4, 100);
         {
